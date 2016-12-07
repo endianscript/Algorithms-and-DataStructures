83d7860086d08427059c23432a03e3fa43ad508a
Pradeep Suresh <endianscript@gmail.com>
2016-12-07 07:02:46 -0800

    Tree DataStructure

---
+++ Tree/binary_tree.py
@@ -0,0 +1,9 @@
+class BinaryNode:
+    def __init__(self):
+        self.right = None
+        self.value = None
+        self.left = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
